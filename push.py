a908c424a34f72b560936155e38d5b565c7df080
Jason Ioannides <jason.ioannides@gmail.com>
2018-08-07 10:49:45 -0400

    building a properly formatted post body

--- push.py
+++ push.py
@@ -24,29 +24,34 @@ web_hook = args.hook
 branch = "master"
 
 payload = {
-    "resources": [
-        {
-            "path": "",
-            "branch": "", 
-            "revision": "",
-            "content": ""
-        }
-    ],
+    "resources": []
 }
 
 if args.branch:
     branch = args.branch
 
 if args.path:
-    # for path in args.path:
-    #     payload['resources'].append(path)
-    with open(os.path.join(args.path)) as stream:
-        try:
-            tree = etree.parse(stream)
-            xml_string = etree.tostring(tree)
-            print(xml_string)
-        except etree.ParseError as exc:
-            print(exc)
+    for root, dirs, files in os.walk(args.path):
+        for filename in files:
+            if filename == "unit.xml" or "input.xml":
+                new_resource = {
+                    "path" : "",
+                    "branch" : "",
+                    "revision" : "",
+                    "content" : ""
+                }
+                with open(os.path.join(args.path + filename)) as stream:
+                    try:
+                        tree = etree.parse(stream)
+                        xml_string = etree.tostring(tree)
+                    except etree.ParseError as exc:
+                        print(exc)
+                new_resource["path"] = filename
+                new_resource["branch"] = args.branch
+                new_resource["content"] = xml_string
+                payload["resources"].append(new_resource)
+
+   
         
 
 if args.config:
